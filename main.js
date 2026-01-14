76bc04ce6876380cb416a9a655598f773ec06b04
Inma <inmllomor@alu.edu.gva.es>
2026-01-14 12:18:42 +0100

    Comprobar que el GitHub Pages funcione, por ello creación de nuevos archivos con estilo css, uso de js y php

---
+++ main.js
@@ -0,0 +1,4 @@
+window.onload= function(){
+    console.log("GitHub Pages está utilizando el JS correctamente");
+    alert("¡Web desplegada con éxito!");
+}
---
+++ prueba.php
@@ -0,0 +1,3 @@
+<?php
+echo "Esto es código PHP";
+?>
